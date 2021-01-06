300792f17e5f5ce20e75a1d1fec0bcfaa4c409f3
Borglin <borglz1@medtronic.com>
2021-01-05 18:29:50 -0800

    Added marker for LA

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -9,7 +9,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a> contributors, href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox/streets-v11',
+        //id: 'mapbox/streets-v11',
         //tileSize: 512,
         //zoomOffset: -1,
         accessToken: API_KEY
